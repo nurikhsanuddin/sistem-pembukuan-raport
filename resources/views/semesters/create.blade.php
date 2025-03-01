9c27c9c366563af22631c7d7b719ca27822d082b
Muhammad Nur Ikhsanuddin <m.nurikhsanuddin@gmail.com>
2025-03-01 18:33:24 +0700

    Update create.blade.php

--- resources/views/semesters/create.blade.php
+++ resources/views/semesters/create.blade.php
@@ -8,7 +8,7 @@
                     </h2>
 
                     @if (session('success'))
-                        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4">
+                        <div class="bg-green-100 border mt-1 border-green-400 text-green-700 px-4 py-3 rounded relative mb-4">
                             {{ session('success') }}
                         </div>
                     @endif
